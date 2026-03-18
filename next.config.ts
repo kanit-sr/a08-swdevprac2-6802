608e3d2bf166434c096726b098f8ccf25e051192
Kanit <stu33074@gmail.com>
2026-03-18 21:09:40 +0700

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,27 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["drive.google.com"],
   },
+  async headers() {
+    return [
+      {
+        // matching all venue routes
+        source: "/venue/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
